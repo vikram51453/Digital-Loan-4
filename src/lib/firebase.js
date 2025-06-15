8c5869c52d68ffe1e4f27e05ae8a774878d87443
vikram <vikramvediyappan20@gmail.com>
2025-06-16 00:05:52 +0530

    update files

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,14 +1,14 @@
-// lib/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArRI-CwsK16KBlfatgTScKiVXEwJE9vFk",
-  authDomain: "digital-loan-24cec.firebaseapp.com",
-  projectId: "digital-loan-24cec",
-  storageBucket: "digital-loan-24cec.appspot.com",
-  messagingSenderId: "584034948291",
-  appId: "1:584034948291:web:003cdccc5f2c3e4e8abe76",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
