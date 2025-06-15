04e2810a3362d5a5c454d781c89bcce8fd33fd06
vikram <vikramvediyappan20@gmail.com>
2025-06-15 16:33:32 +0530

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+// File: pages/_app.js
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react"; // <-- Import Vercel Analytics
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics /> {/* <-- This activates analytics */}
+    </>
+  );
 }
